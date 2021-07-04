aecc34bc07cda794a66122396cafb182ea93f4c1
Anwesh43 <anweshthecool0@gmail.com>
2021-07-04 15:24:19 +0530

    adding constants and libraries

--- linecatchlinerotview/src/main/java/com/example/linecatchlinerotview/LineCatchLineRotView.kt
+++ linecatchlinerotview/src/main/java/com/example/linecatchlinerotview/LineCatchLineRotView.kt
@@ -1,2 +1,26 @@
 package com.example.linecatchlinerotview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#004D40",
+    "#FFD600",
+    "#00C853",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 7.8f
+val delay : Long = 20
+val deg : Float = 180f
+val backColor : Int = Color.parseColor("#BDBDBD")
