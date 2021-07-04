5100ab60b0be6884b623f423ac5a32ba36f9e232
Anwesh43 <anweshthecool0@gmail.com>
2021-07-04 15:31:27 +0530

    adding Canvas extensionn methods

--- linecatchlinerotview/src/main/java/com/example/linecatchlinerotview/LineCatchLineRotView.kt
+++ linecatchlinerotview/src/main/java/com/example/linecatchlinerotview/LineCatchLineRotView.kt
@@ -28,3 +28,30 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) *  n
+
+fun Canvas.drawLineCatchLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, -h / 2 - size + (h / 2) * sc2)
+        drawLine(0f, 0f, 0f, size, paint)
+        restore()
+    }
+    drawLine(-size * 0.5f * sc1, 0f, size * 0.5f * sc1, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawLCLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineCatchLineRot(scale, w, h, paint)
+}
