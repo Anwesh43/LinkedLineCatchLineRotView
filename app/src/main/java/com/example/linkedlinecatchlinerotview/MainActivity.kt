ceecfbb586c40af24dc2ebbefeef0a87ade84ef2
Anwesh43 <anweshthecool0@gmail.com>
2021-07-04 16:28:50 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedlinecatchlinerotview/MainActivity.kt
+++ app/src/main/java/com/example/linkedlinecatchlinerotview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedlinecatchlinerotview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.linecatchlinerotview.LineCatchLineRotView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineCatchLineRotView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
